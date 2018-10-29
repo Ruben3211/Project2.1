9d13835cea54ea42bf9e4120d778e5919608cb4c
codecasper <c.j.van.der.louw@st.hanze.nl>
2018-10-29 11:24:15 +0100

    testje

--- Python/view/gegevens.py
+++ Python/view/gegevens.py
@@ -1,56 +1,43 @@
-import smbus
 
-# Module variables
-i2c_ch = 1
-bus = None
 
-# TMP102 address on the I2C bus
-i2c_address = 0x48
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import gegevens as tmp102
 
-# Register addresses
-reg_temp = 0x00
-reg_config = 0x01
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
 
-# Calculate the 2's complement of a number
-def twos_comp(val, bits):
-    if (val & (1 << (bits - 1))) != 0:
-        val = val - (1 << bits)
-    return val
+# Initialize communication with TMP102
+tmp102.init()
 
-# Read temperature registers and calculate Celsius
-def read_temp():
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
 
-    global bus
+    # Read temperature (Celsius) from TMP102
+    temp_c = round(tmp102.read_temp(), 2)
 
-    # Read temperature registers
-    val = bus.read_i2c_block_data(i2c_address, reg_temp, 2)
-    temp_c = (val[0] << 4) | (val[1] >> 5)
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(temp_c)
 
-    # Convert to 2s complement (temperatures can be negative)
-    temp_c = twos_comp(temp_c, 12)
+    # Limit x and y lists to 20 items
+    xs = xs[-20:]
+    ys = ys[-20:]
 
-    # Convert registers value to temperature (C)
-    temp_c = temp_c * 0.0625
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
 
-    return temp_c
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('TMP102 Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
 
-# Initialize communications with the TMP102
-def init():
-
-    global bus
-
-    # Initialize I2C (SMBus)
-    bus = smbus.SMBus(i2c_ch)
-
-    # Read the CONFIG register (2 bytes)
-    val = bus.read_i2c_block_data(i2c_address, reg_config, 2)
-
-    # Set to 4 Hz sampling (CR1, CR0 = 0b10)
-    val[1] = val[1] & 0b00111111
-    val[1] = val[1] | (0b10 << 6)
-
-    # Write 4 Hz sampling back to CONFIG
-    bus.write_i2c_block_data(i2c_address, reg_config, val)
-
-    # Read CONFIG to verify that we changed it
-    val = bus.read_i2c_block_data(i2c_address, reg_config, 2)
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()
