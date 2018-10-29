697cfb33e34a6158d32b834add16d453206da085
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-10-29 09:23:44 +0100

    het begin

---
+++ C/main.c
@@ -0,0 +1,18 @@
+/*
+ * project.c
+ *
+ * Created: 29-10-2018 09:09:30
+ * Author : ruben
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
