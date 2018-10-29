d197c2bb179e9c5c11419bab5eb0f9993227f6a8
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-10-29 09:28:22 +0100

    Begin van de main

--- C/main.c
+++ C/main.c
@@ -2,17 +2,33 @@
  * project.c
  *
  * Created: 29-10-2018 09:09:30
- * Author : ruben
+ * Author :Ruben,Yaimon,Casper en Kevin
  */ 
-
 #include <avr/io.h>
+#define F_CPU 16E6
 
+#include "AVR_TTC_scheduler.h"
+
+
+// Zet gier alles wat geïnitialiseerd moet worden.
+void setup(){
+	
+	SCH_Init_T1(); // Schedular initialiseren 
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	setup(); // roep de setup functie aan
+// Zet hier onder alle taken die van af de start al moeten draaien
+
+
+
+	SCH_Start(); // Enable Schedular
+	
+	
     while (1) 
     {
+		SCH_Dispatch_Tasks();
     }
 }
 
