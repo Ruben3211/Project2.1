d7cc5f3fc916b78dae81e5e0ab43b5afce2395d9
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-10-29 13:52:28 +0100

    Send en recieve klaar
    De baurate is nog niet perse goed

---
+++ C/serial.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+#include "serial.h"
+void stuur(uint8_t data){
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0 = data;
+	
+}
+
+void ontvang(){
+	loop_until_bit_is_set(UCSR0A, RXC0);
+	return UDR0
+}
---
+++ C/serial.h
@@ -0,0 +1,5 @@
+#ifndef dev_serial_h
+#define dev_serial_h
+void stuur();
+void ontvang();
+#endif
