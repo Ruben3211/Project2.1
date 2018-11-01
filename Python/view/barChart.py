5ee1b94b4d9143740a379a9b736e0de7f741bcad
codecasper <c.j.van.der.louw@st.hanze.nl>
2018-11-01 11:08:16 +0100

    grafiek

--- Python/view/barChart.py
+++ Python/view/barChart.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 plt.axis([0, 10, 0, 10])
 
 for i in range(100):
-    y = np.random.randint(10)
+    y = np.random.randint(1000)
     plt.scatter(i/10, y)
-    plt.pause(0.05)
+    plt.pause(0.5)
 
 plt.show()
