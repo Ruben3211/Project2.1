f62ba6a88578fb29c401411ce31d80b11664c8c5
Ruben Bonga <r.a.bonga@st.hanze.nl>
2018-11-02 12:33:29 +0100

    kan nu ontvangen

--- Python/model/system.py
+++ Python/model/system.py
@@ -1,28 +1,39 @@
 import serial
 import struct
-# class system:
-#     def __init__(self,name ,type):
-#         self.name
-#         self.type
-#         self.port
-#     def connect(self):
-#         ser = serial.Serial(port='COM3', baudrate=9600, bytesize=8, parity='PARITY_NONE', stopbits='STOPBITS_TWO', xonxoff=False)
-#
-#         print('verbonden met' )
-#         return ser
-#
-#     def open_screen(self):
-#
-#     def close_screen(self):
+
+class system:
+    def __init__(self, naam ,type, port):
+        self.naam = naam
+        self.type = type
+        self.port = port
+        self.ser = self.connect()
+
+    def connect(self):
+        ser = serial.Serial(port=self.port, baudrate=9600, bytesize=8, stopbits=2, xonxoff=False)
+        print('verbonden met', self.naam)
+        return ser
+
+    def open_screen(self):
+        self.ser.write(struct.pack('>B', 1))
+
+    def close_screen(self):
+        self.ser.write(struct.pack('>B', 2))
+
+    def ontvang(self):
+        nummer = self.ser.readline(2)
+        return nummer
 
 
-ser = serial.Serial(port='COM3', baudrate=9600  ,stopbits=2, xonxoff=False)
-print(ser)
+unit = system('test','2','com3')
+print(unit)
 while True:
+
     nummer = int(input("voer hier het commando in"))
     if nummer == 1:
-        ser.write(struct.pack('>B', 1))
+        unit.open_screen()
 
     elif nummer == 2:
-        ser.write(struct.pack('>B', 2))
+        unit.close_screen()
 
+    elif nummer == 3:
+        print(unit.ontvang())
