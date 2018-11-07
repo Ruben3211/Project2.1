cd4bf7bdb30072fecd3fff46015504df47d75673
codecasper <c.j.van.der.louw@st.hanze.nl>
2018-11-07 09:38:56 +0100

    commit

--- Python/model/jeloambo.sql
+++ Python/model/jeloambo.sql
@@ -18,12 +18,6 @@ CREATE TABLE `j_units` (
   `port` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `j_units` (`id`, `name`, `type`, `sensitivity`, `measure_freq`, `share_freq`, `datetime_added`, `manual`, `port`) VALUES
-(1, 'arduino', 100, 2, 34, 33, '2018-11-02 00:00:00', 3, 'COM4'),
-(4, 'arduino', 100, 2, 34, 33, '2018-11-02 00:00:01', 3, 'COM4'),
-(5, '1', 1, 1, 1, 1, '2018-11-05 11:54:43', 1, '1'),
-(14, 'Muis', 103, 12, 23232, 2342, '2018-11-05 12:03:27', 33, 'COM35');
-
 CREATE TABLE `j_values` (
   `value` double NOT NULL,
   `datetime` datetime NOT NULL,
