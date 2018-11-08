5c7d0cc6d2493203401b379d0032b9e6ce459cb3
codecasper <c.j.van.der.louw@st.hanze.nl>
2018-11-08 14:02:12 +0100

    database comments

--- Python/model/database.py
+++ Python/model/database.py
@@ -1,23 +1,25 @@
 import mysql
 from mysql import connector
 
+
 class Database:
     def select(self, query, parameters=()):
         """
         Functie waarmee SELECT statement kan worden uitgevoerd.
         :param query: De query die uitgevoerd moet worden.
+        :param parameters: parameters voor de query.
         :return: Het resultaat van de query.
         """
         cursor = self.sql.cursor()
 
         try:
             cursor.execute(query, parameters)
-        except (mysql.connector.errors.ProgrammingError) as e:
+        except mysql.connector.errors.ProgrammingError as e:
             print("SQL:", e)
 
         try:
             result = cursor.fetchall()
-        except (mysql.connector.errors.InterfaceError) as e:
+        except mysql.connector.errors.InterfaceError as e:
             print("SQL:", e)
         cursor.close()
         if 'result' in locals():
@@ -26,7 +28,7 @@ class Database:
     def done(self, n):
         """
         Functie die kijkt of query goed is uitgevoerd.
-        :param n: Aantal rijen dat de database teruggeeft na het uitvoeren van een query
+        :param n: Aantal rijen dat de database teruggeeft na het uitvoeren van een query.
         :return: True als er iets is uitgevoerd, False als er niks is uitgevoerd
         """
         if n == 0:
@@ -45,7 +47,7 @@ class Database:
         try:
             cursor.execute(query, parameters)
             self.sql.commit()
-        except (connector.errors.ProgrammingError) as e:
+        except connector.errors.ProgrammingError as e:
             print("SQL:", e)
         return self.done(cursor.rowcount)
 
@@ -53,6 +55,7 @@ class Database:
         """
         Functie waarmee data uit de database kan worden verwijderd.
         :param query: Query om data te verwijderen.
+        :param parameters: parameters voor de query
         """
         self.script(query, parameters)
 
@@ -60,39 +63,39 @@ class Database:
         """
         Functie waarmee data kan worden geupdate die al in de database staan.
         :param query: Update query.
+        :param parameters: parameters voor de query.
         """
         self.script(query, parameters)
 
     def script(self, query, parameters=()):
         """
         Functie waarmee een sql script kan worden uitgevoerd.
-        :param query: sql script
-        :return: Aantal rijen die zijn aangepast
+        :param query: sql script.
+        :return: Aantal rijen die zijn aangepast.
+        :param parameters: parameters voor de query.
         """
         cursor = self.sql.cursor()
         try:
             cursor.execute(query, parameters)
             self.sql.commit()
-        except (mysql.connector.errors.ProgrammingError) as e:
+        except mysql.connector.errors.ProgrammingError as e:
             print("SQL:", e)
         return self.done(cursor.rowcount)
 
-    def createDB(self):
+    def create_db(self):
         """
         Functie dat een sql-script uitvoerd wanneer de database nog niet bestaat.
-        :return:
         """
         fd = open('jeloambo.sql', 'r', encoding="utf-8")
-        sqlFile = fd.read()
+        sqlfile = fd.read()
         fd.close()
-        commands = sqlFile.replace("\n", "").split(";")
+        commands = sqlfile.replace("\n", "").split(";")
         for command in commands:
             self.script(command)
 
-    def checkDB(self):
+    def check_db(self):
         """
         Functie die checkt of de benodigde database al bestaat.
-        :return:
         """
         query = "SHOW DATABASES"
         db_list = self.select(query)
@@ -102,18 +105,18 @@ class Database:
         if "jeloambo" in l:
             print("SQL: Database bestaat")
         elif "jeloambo" not in l:
-            self.createDB()
+            self.create_db()
             print("SQL :Database is gecreeeërd")
         else:
             print("SQL: Er is iets mis gegaan")
 
     def __init__(self):
         """
-        Maakt een connectie met de database
+        Maakt een connectie met de database.
         """
         self.sql = mysql.connector.connect(host="localhost", user="root", passwd="")
-        # Controlleren of de database al bestaat
+        # Controlleren of de database al bestaat.
         # zo niet dan wordt deze aangemaakt.
-        self.checkDB()
-        # Gebruik maken van de database jeloambo
+        self.check_db()
+        # Gebruik maken van de database jeloambo.
         self.script("USE jeloambo")
