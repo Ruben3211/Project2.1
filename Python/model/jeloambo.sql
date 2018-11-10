e15b5a5890ac6c95604fda3baad18ce0aa5a7e4d
codecasper <c.j.van.der.louw@st.hanze.nl>
2018-11-10 14:27:10 +0100

    sql script aangepast

--- Python/model/jeloambo.sql
+++ Python/model/jeloambo.sql
@@ -8,18 +8,16 @@ USE `jeloambo`;
 
 CREATE TABLE `j_units` (
   `id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `type` int(11) NOT NULL,
-  `sensitivity` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `type` int(2) NOT NULL,
+  `port` varchar(20) NOT NULL,
   `measure_freq` int(11) NOT NULL,
-  `share_freq` int(11) NOT NULL,
-  `datetime_added` datetime NOT NULL,
-  `manual` smallint(6) NOT NULL,
-  `port` varchar(16) NOT NULL
+  `sensitivity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `j_values` (
-  `value` double NOT NULL,
+  `value_id` int(11) NOT NULL,
+  `value` int(11) NOT NULL,
   `datetime` datetime NOT NULL,
   `unit_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -28,10 +26,10 @@ ALTER TABLE `j_units`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `j_values`
-  ADD PRIMARY KEY (`unit_id`,`datetime`);
+  ADD PRIMARY KEY (`value_id`);
 
 ALTER TABLE `j_units`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 ALTER TABLE `j_values`
   ADD CONSTRAINT `values_unit_FK` FOREIGN KEY (`unit_id`) REFERENCES `j_units` (`id`) ON DELETE CASCADE;
